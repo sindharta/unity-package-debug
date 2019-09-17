2529614f6f062786a2ff19a69792b5bc60b4abd4
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2019-09-17 12:04:54 +0900

    feat: added several utility functions (#2)
    * PackageEditorUtility: to get package folder paths
    * CollectionUtility: getting the first element which has a certain criteria (passed as an argument)
    * FileUtility: deleting and copying files
    
    Also created a project using symlink to make it easier to test the package

---
+++ Editor/PackageEditorUtility.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace org.shin.package_debug.Editor {
+
+public class PackageEditorUtility 
+{
+    public static string[] GetPackagesPaths() {
+        if (m_getPackagesPathsMethod == null) {
+            Assembly assembly = typeof(UnityEditor.PackageManager.Client).Assembly;
+            System.Type[] types = assembly.GetTypes();
+
+            System.Type folderType = CollectionUtility.GetFirst(types, 
+                (t) => { return (t.FullName == "UnityEditor.PackageManager.Folders"); }
+            );
+
+            if (null!=folderType) {
+                m_getPackagesPathsMethod = folderType.GetMethod("GetPackagesPaths", BindingFlags.Public | BindingFlags.Static);
+            }
+
+            //error check
+            if (null == m_getPackagesPathsMethod) {
+                return null;
+            }
+        }
+        return (string[]) m_getPackagesPathsMethod.Invoke(null, null);
+    }
+
+    internal static MethodInfo m_getPackagesPathsMethod = null;
+
+}
+
+} //end namespace
---
+++ Runtime/FileUtility.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace org.shin.package_debug {
+
+public static class FileUtility
+{
+
+    public static void DeleteAll(DirectoryInfo di) {
+        try {
+            foreach (FileInfo file in di.EnumerateFiles()) {
+                file.Delete(); 
+            }
+            foreach (DirectoryInfo dir in di.EnumerateDirectories()) {
+                dir.Delete(true); 
+            }
+        } catch {
+            Debug.LogError("Error when trying to delete: " + di.FullName);
+        }
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    //Copy Recursive
+    public static void CopyAll(string sourceDir, string targetDir)
+    {
+        Directory.CreateDirectory(targetDir);
+
+        DirectoryInfo sourceDI = new DirectoryInfo(sourceDir);
+        foreach (FileInfo file in sourceDI.EnumerateFiles()) {
+            File.Copy(file.FullName, Path.Combine(targetDir, file.Name));
+        }
+        foreach (DirectoryInfo dir in sourceDI.EnumerateDirectories()) {
+            CopyAll(dir.FullName, Path.Combine(targetDir, dir.Name));
+        }
+
+    }
+}
+
+} //end namespace

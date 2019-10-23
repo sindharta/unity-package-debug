8b45c15a9a575e461c5bf7635c56811f9eb1cc5f
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-10-23 19:22:01 +0900

    Add an utility function to Wait for Coroutine

---
+++ Runtime/UnityUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class UnityUtility  {
+	public static void WaitCoroutine(IEnumerator coroutineFunc) {
+		while (coroutineFunc.MoveNext ()) {
+            if (null == coroutineFunc.Current)
+                continue;
+            
+			IEnumerator yieldedFunc = coroutineFunc.Current as IEnumerator;
+            if (null == yieldedFunc) {
+                Debug.LogError("Can't cast to IEnumerator: " + coroutineFunc.Current.ToString() );
+                return;
+            }
+			WaitCoroutine (yieldedFunc);
+		}
+	}
+}

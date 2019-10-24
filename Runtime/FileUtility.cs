e9c22166625c9eb3e1d7850e938144c4ba96cf4a
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-10-24 12:03:31 +0900

    Add MoveFolder and ReplaceTextInFileUsingMemory

--- Runtime/FileUtility.cs
+++ Runtime/FileUtility.cs
@@ -1,12 +1,20 @@
 ﻿using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace org.shin.package_debug {
 
-public static class FileUtility
-{
+public static class FileUtility {
 
-    public static void DeleteAll(DirectoryInfo di) {
+//---------------------------------------------------------------------------------------------------------------------
+
+    public static bool DeleteFolder(string path) {
+        return FileUtility.DeleteFolder(new DirectoryInfo(path));        
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public static bool DeleteFolder(DirectoryInfo di) {
         try {
             foreach (FileInfo file in di.EnumerateFiles()) {
                 file.Delete(); 
@@ -15,26 +23,70 @@ public static class FileUtility
                 dir.Delete(true); 
             }
         } catch {
-            Debug.LogError("Error when trying to delete: " + di.FullName);
+            Debug.LogError("[PackageDebug] Error deleting files/folders inside : " + di.FullName);
+            return false;
+        }        
+
+        try {
+            di.Delete(true);
+        } catch {
+            Debug.LogError("[PackageDebug] Error deleting delete: " + di.FullName);
+        }        
+        return true;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public static void CopyFolder(string srcDir, string targetDir, bool overwrite) {
+        Directory.CreateDirectory(targetDir);
+
+        DirectoryInfo sourceDI = new DirectoryInfo(srcDir);
+        foreach (FileInfo file in sourceDI.EnumerateFiles()) {
+            File.Copy(file.FullName, Path.Combine(targetDir, file.Name), overwrite);
+        }
+        foreach (DirectoryInfo dir in sourceDI.EnumerateDirectories()) {
+            CopyFolder(dir.FullName, Path.Combine(targetDir, dir.Name), overwrite);
+        }
+
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    //Files that already exists in targetDir will be overwritten
+    public static void MoveFolder(string srcDir, string targetDir)
+    {
+        Directory.CreateDirectory(targetDir);
+        string[] srcFiles = Directory.GetFiles(srcDir, "*.*", SearchOption.AllDirectories);
+
+        foreach(string srcFile in srcFiles) {
+            FileInfo fileInfo = new FileInfo(srcFile);
+            string dest = Path.Combine(targetDir, fileInfo.Name);
+            if (File.Exists(dest)) {
+                File.Delete(dest);
+            }
+
+            fileInfo.MoveTo(dest);
         }
     }
 
 //---------------------------------------------------------------------------------------------------------------------
 
-    //Copy Recursive
-    public static void CopyAll(string sourceDir, string targetDir)
+    public static bool ReplaceTextInFileUsingMemory(string path, string regex, string newStr, 
+        RegexOptions options = RegexOptions.None) 
     {
-        Directory.CreateDirectory(targetDir);
-
-        DirectoryInfo sourceDI = new DirectoryInfo(sourceDir);
-        foreach (FileInfo file in sourceDI.EnumerateFiles()) {
-            File.Copy(file.FullName, Path.Combine(targetDir, file.Name));
+        if (!File.Exists(path)) {
+            Debug.LogError("[PackageDebug] File doesn't exist: " + path);
+            return false;
         }
-        foreach (DirectoryInfo dir in sourceDI.EnumerateDirectories()) {
-            CopyAll(dir.FullName, Path.Combine(targetDir, dir.Name));
-        }
+            
 
+        string text = File.ReadAllText(path);
+        string replacedText = System.Text.RegularExpressions.Regex.Replace(text, regex, newStr, options);
+
+        File.WriteAllText(path, replacedText);
+        return true;
     }
+
 }
 
-} //end namespace
+}//end namespace

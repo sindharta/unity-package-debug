f3120c7bb9b575f57e7d4933a9f85ed09047a961
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-10-24 12:17:01 +0900

    untabify

--- Runtime/UnityUtility.cs
+++ Runtime/UnityUtility.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 public static class UnityUtility  {
 
     //[Note-sin: 2019-10-24] May not work for all Unity built-in coroutines
-	public static void WaitCoroutine(IEnumerator coroutineFunc) {
-		while (coroutineFunc.MoveNext ()) {
+    public static void WaitCoroutine(IEnumerator coroutineFunc) {
+        while (coroutineFunc.MoveNext ()) {
             if (null == coroutineFunc.Current)
                 continue;
             
-			IEnumerator yieldedFunc = coroutineFunc.Current as IEnumerator;
+            IEnumerator yieldedFunc = coroutineFunc.Current as IEnumerator;
             if (null == yieldedFunc) {
                 Debug.LogError("Can't cast to IEnumerator: " + coroutineFunc.Current.ToString() );
                 return;
             }
-			WaitCoroutine (yieldedFunc);
-		}
-	}
+            WaitCoroutine (yieldedFunc);
+        }
+    }
 }
